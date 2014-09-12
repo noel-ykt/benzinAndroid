2469425ae16f22133044349e38ddce83313a7f4b
noel-ykt <noel.ykt@gmail.com>
2014-09-12 21:44:27 +1000

    Updated. Get запрос к API работает

--- app/src/main/java/com/tabalab/benzinyakutsk/MainActivity.java
+++ app/src/main/java/com/tabalab/benzinyakutsk/MainActivity.java
@@ -1,7 +1,6 @@
 package com.tabalab.benzinyakutsk;
 
 import android.app.Activity;
-import android.app.ListActivity;
 import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.DialogInterface;
@@ -10,9 +9,7 @@ import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
-import android.widget.ListAdapter;
 import android.widget.ListView;
-import android.widget.SimpleAdapter;
 
 import com.tabalab.benzinyakutsk.adapter.MainItemAdapter;
 import com.tabalab.benzinyakutsk.model.Company;
@@ -21,11 +18,10 @@ import com.tabalab.benzinyakutsk.model.Type;
 
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
-import org.apache.http.NameValuePair;
+import org.apache.http.StatusLine;
 import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.HttpClient;
-import org.apache.http.client.entity.UrlEncodedFormEntity;
-import org.apache.http.client.methods.HttpPost;
+import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -37,7 +33,6 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 
 
@@ -53,40 +48,49 @@ public class MainActivity extends Activity {
     private static final String FUEL = "Fuel";
     private static final String TREAD = "Tread";
 
-    ArrayList<HashMap<String, String>> jsonlist = new ArrayList<HashMap<String, String>>();
+    List<ListItem> itemsResult = new ArrayList<ListItem>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        itemList = (ListView) findViewById(R.id.listView);
-
-        List<ListItem> items = initData();
-        MainItemAdapter adapter = new MainItemAdapter(this, items);
-        itemList.setAdapter(adapter);
-
         new ProgressTask().execute();
     }
 
-    private List<ListItem> initData() {
-        List<ListItem> result = new ArrayList<ListItem>();
+    private void initData(String jsonString) {
+        try {
+            JSONArray jsonArray = new JSONArray(jsonString);
+            for (int i = 0; i < jsonArray.length(); i++) {
+                ArrayList<Company> companies = new ArrayList<Company>();
+                try {
+                    JSONObject jsonObject = jsonArray.getJSONObject(i);
+                    //Companies
+                    JSONArray jsonCompanies = jsonObject.getJSONArray("companies");
+                    for (int j = 0; j < jsonCompanies.length(); j++) {
+                        JSONObject jsonCompany = jsonCompanies.getJSONObject(j);
+                        Company company = new Company(jsonCompany.getInt("id"), jsonCompany.getString("name"));
+                        companies.add(company);
+                    }
+                    //Type
+                    JSONObject jsonType = jsonObject.getJSONObject("type");
+                    Type type = new Type(jsonType.getInt("id"), jsonType.getString("name"), jsonType.getString("description"));
+                    //Price
+                    String price = jsonObject.getString("price");
 
-        ArrayList<Company> companies = new ArrayList<Company>();
-        Company company1 = new Company(1, "SINET");
-        Company company2 = new Company(2, "ЯТЭК");
-        companies.add(company1);
-        companies.add(company2);
+                    ListItem item = new ListItem(type, companies, price);
+                    itemsResult.add(item);
+                } catch (Exception ex) {
+                    Log.d("Parse Json Object", ex.getLocalizedMessage());
+                }
+            }
+        } catch (JSONException ex) {
+            Log.e("JSONException", "Error: " + ex.toString());
+        }
 
-        Type type1 = new Type(1, "ДТ", "Дизельное топливо");
-        ListItem item1 = new ListItem(type1, companies, "35.40");
+        itemList = (ListView) findViewById(R.id.listView);
 
-        Type type2 = new Type(1, "Р-92", "Регуляр");
-        ListItem item2 = new ListItem(type2, companies, "35.60");
-
-        result.add(item1);
-        result.add(item2);
-        return result;
+        MainItemAdapter adapter = new MainItemAdapter(this, itemsResult);
+        itemList.setAdapter(adapter);
     }
 
     public void goToView(View v) {
@@ -100,7 +104,7 @@ public class MainActivity extends Activity {
     private class ProgressTask extends AsyncTask<String, String, Void> {
         private ProgressDialog dialog = new ProgressDialog(MainActivity.this);
         InputStream inputStream;
-        String result = "";
+        String responseResult = "[]";
 
         protected void onPreExecute() {
             this.dialog.setMessage("Progress start");
@@ -114,32 +118,23 @@ public class MainActivity extends Activity {
 
         @Override
         protected void onPostExecute(Void aVoid) {
-            try {
-                JSONArray jsonArray = new JSONArray(result);
-                for (int i = 0; i < jsonArray.length(); i++) {
-                    JSONObject jsonObject = jsonArray.getJSONObject(i);
-
-
-                }
-            } catch (JSONException ex) {
-                Log.e("JSONException", "Error: " + ex.toString());
-            }
+            this.dialog.hide();
+            initData(responseResult);
         }
 
         @Override
         protected Void doInBackground(String... strings) {
-            ArrayList<NameValuePair> param = new ArrayList<NameValuePair>();
-
             try {
                 HttpClient httpClient = new DefaultHttpClient();
 
-                HttpPost httpPost = new HttpPost(url);
-                httpPost.setEntity(new UrlEncodedFormEntity(param));
-                HttpResponse httpResponse = httpClient.execute(httpPost);
-                HttpEntity httpEntity = httpResponse.getEntity();
-
-                // Read content & Log
-                inputStream = httpEntity.getContent();
+                HttpGet httpGet = new HttpGet(url);
+                HttpResponse httpResponse = httpClient.execute(httpGet);
+                StatusLine statusLine = httpResponse.getStatusLine();
+                int statusCode = statusLine.getStatusCode();
+                if (statusCode == 200) {
+                    HttpEntity httpEntity = httpResponse.getEntity();
+                    inputStream = httpEntity.getContent();
+                }
             } catch (UnsupportedEncodingException e1) {
                 Log.e("UnsupportedEncodingException", e1.toString());
                 e1.printStackTrace();
@@ -157,13 +152,12 @@ public class MainActivity extends Activity {
             try {
                 BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
                 StringBuilder stringBuilder = new StringBuilder();
-
                 String line;
                 while ((line = bufferedReader.readLine()) != null) {
                     stringBuilder.append(line + '\n');
                 }
                 inputStream.close();
-                result = stringBuilder.toString();
+                responseResult = stringBuilder.toString();
             } catch (Exception e) {
                 Log.e("StringBuilding & BufferedReader", "Error converting result " + e.toString());
             }
